385402f5c0a10d7039f842399b799143549dca74
FE_sunmin <fe.sunmin@gmail.com>
2024-05-23 10:14:31 +0900

    문제풀이

--- practice2-1.js
+++ practice2-1.js
@@ -11,29 +11,57 @@
 // 입력: "banana"
 // 출력: 'a' (혹은 'n')
 
+// 문자열의 각 문자를 순회한다
+// 문자가 객체에 존재하면, 문자의 빈도를 1 증가시킨다
+// 존재하지 않으면 1로 초기화시킨다
+// 빈도 객체의 각 문자를 순회한다
+// 현재 문자의 빈도가 최대 빈도보다 크면, 최대 빈도를 현재 문자의 빈도로 갱신한다
+// 최대 빈도의 문자를 현재 문자로 갱신한다
 
 function mostFrequentChar(s) {
-    // 이곳에 함수 작성을 해주세요.
+  // 이곳에 함수 작성을 해주세요.
+  const frequ = {};
+
+  for (const char of s) {
+    if (frequ[char]) {
+      frequ[char]++;
+    } else {
+      frequ[char] = 1;
+    }
+  }
+
+  let maxCount = 0;
+  let maxChar = null;
+
+  for (const char in frequ) {
+    if (frequ[char] > maxCount) {
+      maxCount = frequ[char];
+      maxChar = char;
+    }
+  }
+
+  return maxChar;
 }
 
 // 테스트 코드
 function testMostFrequentChar() {
-    const testCases = [
-        { input: "banana", expected: ['a', 'n'] },
-        { input: "apple", expected: ['p'] },
-        { input: "mississippi", expected: ['i', 's'] },
-        { input: "aabbcc", expected: ['a', 'b', 'c'] },
-    ];
+  const testCases = [
+    { input: "banana", expected: ["a", "n"] },
+    { input: "apple", expected: ["p"] },
+    { input: "mississippi", expected: ["i", "s"] },
+    { input: "aabbcc", expected: ["a", "b", "c"] },
+  ];
 
-    testCases.forEach(({input, expected}, index) => {
-        try {
-            const result = mostFrequentChar(input);
-            if (!expected.includes(result)) throw new Error(`Expected one of ${expected}, but got ${result}`);
-            console.log(`Test ${index + 1}: Passed`);
-        } catch (error) {
-            console.log(`Test ${index + 1}: Failed - ${error.message}`);
-        }
-    });
+  testCases.forEach(({ input, expected }, index) => {
+    try {
+      const result = mostFrequentChar(input);
+      if (!expected.includes(result))
+        throw new Error(`Expected one of ${expected}, but got ${result}`);
+      console.log(`Test ${index + 1}: Passed`);
+    } catch (error) {
+      console.log(`Test ${index + 1}: Failed - ${error.message}`);
+    }
+  });
 }
 
 // 테스트 함수 호출 : 터미널에 node practice2-1.js 실행
